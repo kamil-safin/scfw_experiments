f8245894b24b17067d9d07490c1c56e887cfcc68
kamil-safin <kamil.95@yandex.ru>
2020-04-12 11:55:45 +0300

    add lloo policy to FW

--- scfw/frank_wolfe.py
+++ scfw/frank_wolfe.py
@@ -2,7 +2,7 @@ import time
 
 import numpy as np
 
-from scfw.alpha_policies import *
+from alpha_policies import *
 
 
 def frank_wolfe(fun_x,
@@ -15,6 +15,10 @@ def frank_wolfe(fun_x,
                 linear_oracle,
                 x_0,
                 FW_params,
+                hess=None,
+                sigma_f=None,
+                diam_X=None,
+                rho=None,
                 alpha_policy='standard',
                 eps=0.001,
                 print_every=100,
@@ -24,6 +28,7 @@ def frank_wolfe(fun_x,
         grad_x -- grad value
         grad_beta -- grad velue for convex combination
         hess_mult_x -- Hessian multiplies by x on both sides
+        hess -- Hessian computation (for lloo)
         extra_fun -- extra function used to transfer to other methods
         Mf -- parameter for self concordence
         nu -- ?
@@ -33,7 +38,6 @@ def frank_wolfe(fun_x,
         eps -- epsilon
         print_every -- print results every print_every steps
     """
-    n = len(x_0)
     lower_bound = float("-inf")
     upper_bound = float("inf")
     criterion = 1e10 * eps
@@ -72,7 +76,7 @@ def frank_wolfe(fun_x,
         elif alpha_policy == 'backtracking':
             extra_param_s = extra_fun(s) #this is a way to know if the gradient is defined on s
             my_func_beta = lambda beta: fun_x(beta*s+(1-beta)*x,beta*extra_param_s*(1-beta)*extra_param_s)[0]
-            alpha, L_last = alpha_L_backtrack(my_func_beta,Q,grad,-delta_x,L_last):
+            alpha, L_last = alpha_L_backtrack(my_func_beta,Q,grad,-delta_x,L_last)
         elif alpha_policy == 'line_search':
             extra_param_s = extra_fun(s) #this is a way to know if the gradient is defined on s
             if min(extra_param_s) == 0: #if 0 it is not defines and beta is adjusted
@@ -84,6 +88,14 @@ def frank_wolfe(fun_x,
         elif alpha_policy == 'icml':
             hess_mult = hess_mult_x(s - x, extra_params)
             alpha = alpha_icml(Gap, hess_mult, -delta_x, Mf, nu)
+        elif alpha_policy == 'lloo':
+            if k == 1:
+                hess_val = hess(x, extra_params)
+                L = max(np.linalg.eigvalsh(hess_val))
+                c = 1 + Mf * diam_X * np.sqrt(L) / 2
+                r = 1
+            hess_func = lambda x: hess(x, extra_params)
+            alpha, r, L, c = alpha_lloo(x, hess_func, r, L, c, Mf, sigma_f, diam_X, rho)
 
         # filling history
         x_hist.append(x)
@@ -124,7 +136,7 @@ def frank_wolfe(fun_x,
                 print(f'criterion = {criterion}')
                 #print(f'grad = {grad}')
                 print(f'grad norm = {norm(grad)}')
-                print(f'min abs dot = {min(abs(dot_product))}')
+                #print(f'min abs dot = {min(abs(dot_product))}')
                 #print(f'x = {x}')
                 #x_nz = x[np.nonzero(x)[0]]
                 #print(f'x non zero: {list(zip(x_nz, np.nonzero(x)[0]))}\n')
