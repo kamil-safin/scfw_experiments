1f57c0ec692d56fdaac3e0eccf3c617c16106265
kamil-safin <kamil.95@yandex.ru>
2020-05-05 14:46:05 +0300

    update for matrix use

--- scfw/phase_retrival.py
+++ scfw/phase_retrival.py
@@ -11,7 +11,7 @@ def norm(x):
 
 def phase_val(A, X, y, trace_sum=None):
     sum_val = 0
-    if trace_sum==None:
+    if trace_sum is None:
         trace_sum=[]
         for y_i, A_i in zip(y, A):
             trace = np.trace(A_i.dot(X)).real
@@ -21,16 +21,21 @@ def phase_val(A, X, y, trace_sum=None):
             trace_sum.append(trace)
             sum_val += -y_i * np.log(trace) + trace
     else:
-        sum_val=-y.dot(log(trace_sum))+sum(trace_sun)
-    return sum_val, trace_sum
+        sum_val=-y.dot(np.log(trace_sum))+sum(trace_sum)
+    return sum_val, np.array(trace_sum)
 
-def phase_gradient(A, X, y, trace_sum):
+def phase_gradient(A, X, y, trace_sum=None):
     sum_val = 0
 #    A_T = np.conjugate(np.transpose(A, axes=(0, 2, 1)))
 #    return np.sum((-y / np.trace(A.dot(X), axis1=1, axis2=2).real)[:, None, None] * A_T + A_T, axis=0)
-    for y_i, A_i,trace_sum_i in zip(y, A,trace_sum):
-        #sum_val += -y_i * np.conjugate(A_i).T / trace_sum_i + np.conjugate(A_i).T
-        sum_val += -y_i * A_i / trace_sum_i + A_i #A_i is hermitian
+    if trace_sum is None:
+        for y_i, A_i in zip(y, A):
+            trace_sum_i = np.trace(A_i.dot(X)).real
+            sum_val += -y_i * A_i / trace_sum_i + A_i
+    else:
+        for y_i, A_i, trace_sum_i in zip(y, A,trace_sum):
+            #sum_val += -y_i * np.conjugate(A_i).T / trace_sum_i + np.conjugate(A_i).T
+            sum_val += -y_i * A_i / trace_sum_i + A_i #A_i is hermitian
     return sum_val
 
 def hess_mult(A, y, S,trace_sum):
@@ -45,11 +50,17 @@ def hess_mult_vec(A, y, S, trace_sum):
         sum_val += y_i * np.conjugate(A_i).T * np.trace(A_i.dot(S)).real / trace_sum_i**2
     return sum_val
 
+def linear_oracle(grad, c):
+    eig_vals, eig_vecs = sc.eigh(grad, eigvals=(0, 0)) # eigh ?
+    u_t = eig_vecs[:, 0]
+    V_t = c * np.dot(u_t.reshape(-1, 1), np.conj(u_t).reshape(1, -1))
+    return V_t
+
 def proj_map(X, c):
     #U, v, UH = np.linalg.svd(X, hermitian=True)
-    v, U=sc.eigh(X) #this should not be projection on the simplex but projection on the simplex with radius c
+    v, U = sc.eigh(X) #this should not be projection on the simplex but projection on the simplex with radius c
     #check for numerical stability
-    n1=sc.norm(X-U@np.diag(v)@np.conjugate(U).T)
+    n1 = sc.norm(X - U @ np.diag(v) @ np.conjugate(U).T)
     #print(X@U-np.diag(v)*U)
     if n1>1e-10:
         print(n1)
